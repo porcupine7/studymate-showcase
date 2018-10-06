950364096ec19a4f436518221bffce6b3bd3b946
porcupine7 <andreas.guetz@students.fh-hagenberg.at>
2018-10-06 12:21:02 +0200

    #AN-12 Added notification channel for Oreo
    - Without channel it is not shown on Android Oreo
    - Channel is only created on Oreo, older Android versions don't recognize it

--- app/src/main/java/at/fhooe/studymate/fragments/TaskPresenter.java
+++ app/src/main/java/at/fhooe/studymate/fragments/TaskPresenter.java
@@ -1,9 +1,12 @@
 package at.fhooe.studymate.fragments;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 
@@ -22,175 +25,189 @@ import at.fhooe.studymate.model.BlockConverter;
  * Class to act upon TaskFragment and model classes
  */
 public class TaskPresenter {
-  private final ExperimentInfo experiment;
-  /**
-   * The block defines which conditions participant get in which order
-   */
-  private final Block block;
-  /**
-   * Index is counted with the progression of trials, increment by one after one trial is done
-   */
-  private int trialIndex;
-  /**
-   * Whether participant currently performs a task, otherwise he reads the task description
-   */
-  private boolean taskIsRunning;
+    private final ExperimentInfo experiment;
+    /**
+     * The block defines which conditions participant get in which order
+     */
+    private final Block block;
+    /**
+     * Index is counted with the progression of trials, increment by one after one trial is done
+     */
+    private int trialIndex;
+    /**
+     * Whether participant currently performs a task, otherwise he reads the task description
+     */
+    private boolean taskIsRunning;
 
-  /**
-   * Constructor to create the task presenter
-   * @param experiment received from server
-   * @param participantInfo ID and block of participant
-   * @param fieldRep
-   */
-  public TaskPresenter(ExperimentInfo experiment, ParticipantInfo participantInfo, String fieldRep) {
-    this.experiment = experiment;
-    RawConditionBundle[][] rawConditionBundles = experiment.getBlocks();
-    BlockConverter blockConverter = new BlockConverter(experiment, rawConditionBundles, fieldRep);
-    int blockIndex = participantInfo.getBlockIndex();
-    block = blockConverter.getBlock(blockIndex);
+    /**
+     * Constructor to create the task presenter
+     *
+     * @param experiment      received from server
+     * @param participantInfo ID and block of participant
+     * @param fieldRep        repetition
+     */
+    public TaskPresenter(ExperimentInfo experiment, ParticipantInfo participantInfo, String fieldRep) {
+        this.experiment = experiment;
+        RawConditionBundle[][] rawConditionBundles = experiment.getBlocks();
+        BlockConverter blockConverter = new BlockConverter(experiment, rawConditionBundles, fieldRep);
+        int blockIndex = participantInfo.getBlockIndex();
+        block = blockConverter.getBlock(blockIndex);
 
-    trialIndex = -1;
-    taskIsRunning = false;
-  }
+        trialIndex = -1;
+        taskIsRunning = false;
+    }
 
-  /**
-   * Load old state, so that fragment can look the same after app went to background
-   *
-   * @param savedState Contains saved index and whether task was running
-   */
-  public void loadState(Bundle savedState) {
-    trialIndex = savedState.getInt("trialIndex");
-    taskIsRunning = savedState.getBoolean("taskRunning");
-  }
+    /**
+     * Load old state, so that fragment can look the same after app went to background
+     *
+     * @param savedState Contains saved index and whether task was running
+     */
+    public void loadState(Bundle savedState) {
+        trialIndex = savedState.getInt("trialIndex");
+        taskIsRunning = savedState.getBoolean("taskRunning");
+    }
 
-  /**
-   * Save trial index and whether task is running
-   *
-   * @param state Android's Bundle
-   */
-  public void saveState(Bundle state) {
-    state.putInt("trialIndex", trialIndex);
-    state.putBoolean("taskRunning", taskIsRunning);
-  }
+    /**
+     * Save trial index and whether task is running
+     *
+     * @param state Android's Bundle
+     */
+    public void saveState(Bundle state) {
+        state.putInt("trialIndex", trialIndex);
+        state.putBoolean("taskRunning", taskIsRunning);
+    }
 
-  /**
-   * @return package name of current app in trial
-   */
-  public String getCurApp() {
-    return getCurTrial().getApp().getPackageName();
-  }
+    /**
+     * @return package name of current app in trial
+     */
+    public String getCurApp() {
+        return getCurTrial().getApp().getPackageName();
+    }
 
-  /**
-   * @return true, when another trial is available. When false experiment is finished
-   */
-  public boolean loadNextTrial() {
-    trialIndex++;
-    taskIsRunning = false;
-    boolean trialAvailable =
-        trialIndex < block.getOrderedConditionBundles().size();
-    return trialAvailable;
-  }
+    /**
+     * @return true, when another trial is available. When false experiment is finished
+     */
+    public boolean loadNextTrial() {
+        trialIndex++;
+        taskIsRunning = false;
+        return trialIndex < block.getOrderedConditionBundles().size();
+    }
 
-  /**
-   * Called when task is started
-   */
-  public void taskStarted() {
-    taskIsRunning = true;
-  }
+    /**
+     * Called when task is started
+     */
+    public void taskStarted() {
+        taskIsRunning = true;
+    }
 
-  /**
-   * Called when task was finished or conceded
-   */
-  public void onTaskEnded() {
-    taskIsRunning = false;
-  }
+    /**
+     * Called when task was finished or conceded
+     */
+    public void onTaskEnded() {
+        taskIsRunning = false;
+    }
 
-  public Task getCurTask() {
-    return getCurTrial().getTask();
-  }
+    public Task getCurTask() {
+        return getCurTrial().getTask();
+    }
 
-  /**
-   * Use first {@link #hasTaskFollowUpQuestionnaire()}
-   * @return the follow up questionnaire
-   */
-  public Questionnaire getFollowUpQuestionnaire() {
-    String questionnaireId = getCurTrial().getTask().getFollowUpQuestionnaireId();
-    return experiment.findQuestionnaireById(questionnaireId);
-  }
+    /**
+     * Use first {@link #hasTaskFollowUpQuestionnaire()}
+     *
+     * @return the follow up questionnaire
+     */
+    public Questionnaire getFollowUpQuestionnaire() {
+        String questionnaireId = getCurTrial().getTask().getFollowUpQuestionnaireId();
+        return experiment.findQuestionnaireById(questionnaireId);
+    }
 
-  /**
-   * @return whether this current task has a followUpQuestionnaire
-   */
-  public boolean hasTaskFollowUpQuestionnaire() {
-    return getFollowUpQuestionnaire() != null;
-  }
+    /**
+     * @return whether this current task has a followUpQuestionnaire
+     */
+    public boolean hasTaskFollowUpQuestionnaire() {
+        return getFollowUpQuestionnaire() != null;
+    }
 
-  /**
-   * @return whether participant sees a questionnaire after all the tasks
-   */
-  public boolean hasPostExpQuestionnaire() {
-    return getPostExpQuestionnaire() != null;
-  }
+    /**
+     * @return whether participant sees a questionnaire after all the tasks
+     */
+    public boolean hasPostExpQuestionnaire() {
+        return getPostExpQuestionnaire() != null;
+    }
 
-  /**
-   * Use first {@link #hasPostExpQuestionnaire()}
-   * @return the post exp questionnaire
-   */
-  public Questionnaire getPostExpQuestionnaire() {
-    return experiment.getPostExpQuestionnaire();
-  }
+    /**
+     * Use first {@link #hasPostExpQuestionnaire()}
+     *
+     * @return the post exp questionnaire
+     */
+    public Questionnaire getPostExpQuestionnaire() {
+        return experiment.getPostExpQuestionnaire();
+    }
 
-  /**
-   * @return trial or condition bundle currently loaded
-   */
-  public ConditionBundle getCurTrial() {
-    return block.getOrderedConditionBundles().get(trialIndex);
-  }
+    /**
+     * @return trial or condition bundle currently loaded
+     */
+    public ConditionBundle getCurTrial() {
+        return block.getOrderedConditionBundles().get(trialIndex);
+    }
 
-  /**
-   * @return whether participant is currently performing the task
-   */
-  public boolean isTaskRunning() {
-    return taskIsRunning;
-  }
+    /**
+     * @return whether participant is currently performing the task
+     */
+    public boolean isTaskRunning() {
+        return taskIsRunning;
+    }
 
-  /**
-   * Identifier for the notification, a new notification with this id will overwrite the old one
-   */
-  public static final int NOTIFICATION_ID = 0;
+    /**
+     * Identifier for the notification, a new notification with this id will overwrite the old one
+     */
+    public static final int NOTIFICATION_ID = 0;
 
-  /**
-   * Sticky jask notification is shown to enable the participant to finish task
-   *
-   * @param task is needed to show correct name and short summary of task
-   */
-  public static void showTaskNotification(Context context, Task task) {
-    NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-    builder.setContentTitle(task.getName());
-    builder.setContentText(task.getShortDescription());
-    builder.setSmallIcon(R.drawable.check_circle);
-    builder.addAction(new android.support.v4.app.NotificationCompat.Action(R.drawable.help_circle,
-        context.getString(R.string.help_notification), createTaskIntent(context, false)));
-    builder.addAction(new android.support.v4.app.NotificationCompat.Action(R.drawable.check_circle,
-        context.getString(R.string.finish_notification), createTaskIntent(context, true)));
-    builder.setAutoCancel(false);
-    builder.setOngoing(true);
-    NotificationManager notificationManager =
-        (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-    notificationManager.notify(NOTIFICATION_ID, builder.build());
-  }
+    private static final String CHANNEL_ID = "at.fhooe.studymate";
 
-  /**
-   * Is triggered when notification is clicked
-   *
-   * @param finished true: finishes task and opens next| false: just show description again
-   * @return opens activity with delay
-   */
-  private static PendingIntent createTaskIntent(Context context, boolean finished) {
-    Intent finishedIntent = new Intent(context, TaskActivity.class);
-    finishedIntent.putExtra(TaskActivity.KEY_TASK_FINISHED, finished);
-    //Different request code for the intents, otherwise it is just overwritten
-    int requestCode = finished ? 0 : 1;
-    return PendingIntent.getActivity(context, requestCode, finishedIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-  }
+    /**
+     * Sticky jask notification is shown to enable the participant to finish task
+     *
+     * @param task is needed to show correct name and short summary of task
+     */
+    public static void showTaskNotification(Context context, Task task) {
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        assert notificationManager != null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Tasks", NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
+        Notification notification = createNotification(context, task);
+        notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
+    private static Notification createNotification(Context context, Task task) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID);
+        builder.setContentTitle(task.getName());
+        builder.setContentText(task.getShortDescription());
+        builder.setSmallIcon(R.drawable.check_circle);
+        builder.addAction(new NotificationCompat.Action(R.drawable.help_circle,
+                context.getString(R.string.help_notification), createTaskIntent(context, false)));
+        builder.addAction(new NotificationCompat.Action(R.drawable.check_circle,
+                context.getString(R.string.finish_notification), createTaskIntent(context, true)));
+        builder.setAutoCancel(false);
+        builder.setOngoing(true);
+        builder.setChannelId(CHANNEL_ID);
+        return builder.build();
+    }
+
+    /**
+     * Is triggered when notification is clicked
+     *
+     * @param finished true: finishes task and opens next| false: just show description again
+     * @return opens activity with delay
+     */
+    private static PendingIntent createTaskIntent(Context context, boolean finished) {
+        Intent finishedIntent = new Intent(context, TaskActivity.class);
+        finishedIntent.putExtra(TaskActivity.KEY_TASK_FINISHED, finished);
+        //Different request code for the intents, otherwise it is just overwritten
+        int requestCode = finished ? 0 : 1;
+        return PendingIntent.getActivity(context, requestCode, finishedIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
 }
